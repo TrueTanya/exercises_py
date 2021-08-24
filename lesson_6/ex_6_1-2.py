87df6e040217ff96ecc3901dc581e9c65a38caff
TrueTanya <ognaryova1@gmail.com>
2021-08-24 22:27:12 +0300

    rename files and lesson_6

---
+++ lesson_6/ex_6_1-2.py
@@ -0,0 +1,12 @@
+#calculate S of the circle
+pi = 3.14
+r = 10
+S = pi * r**2
+print(S)
+
+b = 10 #width of the rectangle
+h = 2 #height of the rectangle
+P = 2 * (b + h)
+print(P)
+b = 6
+print(2 * (b + h))
